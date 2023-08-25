b14a5ce20bcbffb65c1ef67b0defd0076feff30d
lalvarezweb <info@lalvarezweb.es>
2023-08-25 13:52:38 +0200

    Basic SEO tags

--- app.vue
+++ app.vue
@@ -3,3 +3,16 @@
     <NuxtPage/>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'The Rose TechTest',
+  meta: [
+    { name: 'description', content: 'Tech test for job offer in The Rose Studio' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
